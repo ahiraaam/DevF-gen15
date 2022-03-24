861c752223a1e04a002d4e64625596fd2aaf8de5
ahiraaam <ahicruma23@gmail.com>
2022-03-23 20:05:03 -0600

    Calculadora

--- 02-intro-js/Clase8-DOM/login.js
+++ 02-intro-js/Clase8-DOM/login.js
@@ -1,16 +1,39 @@
-function login()
-{
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+let seccionPrincipal = document.getElementById("seccion_principal");
+seccionPrincipal.hidden = true;
+let usuarioActual = null;
 
-    console.log(username,password);
+function login(event) {
+  event.preventDefault();
+  let username = document.getElementById("username");
+  let password = document.getElementById("password").value;
+  let mensaje = document.getElementById("mensaje");
+  console.log(username, password);
 
-    if( username === "carlos.burgueno@gmail.com" && password === "123" )
-    {
-        window.location.href = "index.html";
-        console.log("login hecho");
-    }else
-    {
-        alert("usuario y/o contraseña incorrectos");
+  if (username.value === "a@gmail.com" && password === "123") {
+    //window.location.href = "index.html";
+    let btnLogin = document.getElementById("btn_login");
+    btnLogin.disabled = true;
+    username.readOnly = true;
+    if (usuarioActual != null) {
+      mensaje.style.color = "red";
+      mensaje.innerHTML = "<strong>Ya estás logeado</strong>";
+    } else {
+      mensaje.style.color = "green";
+      mensaje.innerHTML = "<strong>Login exitoso</strong>";
+      seccionPrincipal.hidden = false;
+      let h2 = document.createElement("h2");
+      h2.innerText = username.value;
+      seccionPrincipal.appendChild(h2);
+      usuarioActual = username.value;
     }
+  } else {
+    mensaje.style.color = "red";
+    mensaje.innerText = "Error al iniciar sesión ";
+  }
 }
+
+let btnIngresarDinero = document.getElementById("btn_ingresar_dinero");
+
+btnIngresarDinero.addEventListener("click", function () {
+  alert("Hola");
+});
